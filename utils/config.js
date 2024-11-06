8f8c0628d5d7fc8478732b550336d0a7a8703a4b
KareimGazer <kareimgazar1990@gmail.com>
2024-11-06 10:06:42 +0200

    added default values for the env vars

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const PORT = process.env.PORT
-const TIME_INTERVAL = process.env.TIME_INTERVAL
+const PORT = process.env.PORT || 3000
+const TIME_INTERVAL = process.env.TIME_INTERVAL || 5000
 
 module.exports = { PORT, TIME_INTERVAL }
